b43352f6ab0b8f6a5bdb28497e91df1208aab0f2
ashutosh <mrashutheboy@gmail.com>
2025-12-21 18:57:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div className='parent'>
+       <div className="cards">
+        <img src='https://images.unsplash.com/photo-1668278535123-73de3fecb59e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='bison image'/> 
+       <h1> Hello</h1>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsa.</p>
+       <button>view content</button>
+       </div>
+
+       <div className="cards">
+        <img src='https://images.unsplash.com/photo-1668278535123-73de3fecb59e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='bison image'/> 
+       <h1> Hello</h1>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsa.</p>
+       <button>view content</button>
+       </div>
+
+       <div className="cards">
+        <img src='https://images.unsplash.com/photo-1668278535123-73de3fecb59e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='bison image'/> 
+       <h1> Hello</h1>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsa.</p>
+       <button>view content</button>
+       </div>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
---
+++ src/components/card.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+       <div className="card">
+            <h1>ASHUTOSH SAMAL</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, ducimus saepe ea culpa distinctio consequatur!</p>
+           </div>
+    </div>
+  )
+}
+
+export default Card
